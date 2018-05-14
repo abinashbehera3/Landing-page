e6f98627f37f48cd9b1e69baeba09b6a0725a2bc
abinash <abinashbeherabls@gmail.com>
2018-05-14 11:34:52 +0530

    routing-fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes,RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const ROUTES:Routes =[
+{ path:'',component:LandingPageComponent},
 ]
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AdminRoutingModule } from './admin-routing.module';
+// import { AdminRoutingModule } from './admin-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
@@ -23,7 +23,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
   ],
   imports: [
     BrowserModule,
-    AdminRoutingModule,
+    // AdminRoutingModule,
     AppRoutingModule
   ],
   providers: [],
